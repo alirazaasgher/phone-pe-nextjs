1ac0ef46ea32353902bf32fdf314af92bf3abb86
alirazaasgher <alirazaasgher21@gmail.com>
2025-11-17 20:25:54 +0500

    C:/Program Files/Git/

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import HomeContent from "@/components/HomeContent";
 import { homePageData } from "./services/phones";
-// export const dynamic = 'force-dynamic';
+export const dynamic = 'force-dynamic';
 export default async function Home() {
   const homePageDataResponse = await homePageData();
   return (
