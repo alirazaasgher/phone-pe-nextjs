ef3802a42f95249233060ef5fdce5ed08d15ac7e
alirazaasgher <alirazaasgher21@gmail.com>
2025-11-29 01:51:33 +0500

    url updated

--- src/app/services/phones.js
+++ src/app/services/phones.js
@@ -1,7 +1,7 @@
 // services/phones.ts
 export async function getPhoneById(id) {
   // call your DB or external API directly
-  const res = await fetch(`http://api.mobile42.com/api/phones/${id}`);
+  const res = await fetch(`https://api.mobile42.com/api/phones/${id}`);
   if (!res.ok) return null;
   const json = await res.json();
   return json.data; // return phone object
@@ -9,7 +9,7 @@ export async function getPhoneById(id) {
 
 export async function getPhoneBySlug(slug) {
   // call your DB or external API directly
-  const res = await fetch(`http://api.mobile42.com/api/phones/${slug}`);
+  const res = await fetch(`https://api.mobile42.com/api/phones/${slug}`);
   if (!res.ok) return null;
   const json = await res.json();
   return json.data; // return phone object
@@ -17,7 +17,7 @@ export async function getPhoneBySlug(slug) {
 
 export async function homePageData() {
   // call your DB or external API directly
-  const res = await fetch(`http://api.mobile42.com/api/homepage`);
+  const res = await fetch(`https://api.mobile42.com/api/homepage`);
   const json = await res.json();
 
   return json.data; // return phone object
@@ -25,7 +25,7 @@ export async function homePageData() {
 
 export async function getAllPhoneSlugs() {
   // call your DB or external API directly
-  const res = await fetch(`http://api.mobile42.com/api/getPhoneBySlug`);
+  const res = await fetch(`https://api.mobile42.com/api/getPhoneBySlug`);
   const json = await res.json();
   return json.data; // return phone object
 }
@@ -35,7 +35,7 @@ export async function mobilePageData(filters = [], sortValue) {
     filters, // your parsed filters
     sort: sortValue,
   };
-  const res = await fetch("http://api.mobile42.com/api/phones", {
+  const res = await fetch("https://api.mobile42.com/api/phones", {
     method: "POST", // or "GET" if your backend expects query params
     headers: {
       "Content-Type": "application/json",
@@ -43,7 +43,7 @@ export async function mobilePageData(filters = [], sortValue) {
     },
     body: JSON.stringify(body),
   });
-  console.log(body)
+  console.log(body);
   const json = await res.json();
   return json.data; // return phone object
 }
