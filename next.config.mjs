03c7e57410da8be030cd061f3da6414fbe8a71ce
alirazaasgher <alirazaasgher21@gmail.com>
2025-12-03 21:04:55 +0500

    new changes

--- next.config.mjs
+++ next.config.mjs
@@ -8,13 +8,13 @@ const nextConfig = {
       {
         protocol: "http",
         hostname: "127.0.0.1",
-        port: "",
+        port: "8000",
         pathname: "/storage/**",
       },
       {
         protocol: "http",
         hostname: "localhost",
-        port: "",
+        port: "8000",
         pathname: "/storage/**",
       },
     ],
